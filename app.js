fd984a373b0cc88e0dea5a62b3bba32d1c6b50c2
Moses Martinez <mm.mosesmartinez@gmail.com>
2019-02-13 20:18:50 -0800

    Express App

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => res.send('World!'));
+app.get('/add', (req, res) => res.send('00000!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
